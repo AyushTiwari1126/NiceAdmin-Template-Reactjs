522c9abb2e6d6167011b9e2a667011fba20915cc
AyushTiwari <ayush@wowfinstack.com>
2023-06-13 16:37:58 +0530

    react template

---
+++ src/Pages/Addapplication.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Addapplication() {
+    return (
+        <>
+            <main id="main" class="main">
+                <div class="pagetitle">
+                    <h1>Add Application</h1>
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">Add Application</li>
+                        </ol>
+                    </nav>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Addapplication
---
+++ src/Pages/Processapplication.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Processapplication() {
+    return (
+        <>
+            <main id="main" class="main">
+                <div class="pagetitle">
+                    <h1>Process Application</h1>
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">Process Application</li>
+                        </ol>
+                    </nav>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Processapplication
